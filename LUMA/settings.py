cece5485ea448d1382d13a188d0b8430678e6656
RohithR <rohithr01092003@gmail.com>
2025-03-27 23:18:17 +0530

    allowed host modified

--- LUMA/settings.py
+++ LUMA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e2pqmletjzbpp-j&pu+$_o5$ytv8sma8yafux*-5_jbfkidxyk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-mini-blog-o4my.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
